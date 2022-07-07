54a8f314cd2d4acef44d65a886a19a730d3fc22d
AnumShabbir032 <anumshabbir032@gmail.com>
2022-07-07 19:26:42 +0500

    Javascript-ES6

---
+++ Task 01/app.js
@@ -0,0 +1,32 @@
+function percentage() {
+    // get all subject value
+    let subject1 = document.querySelector("#subject1").value;
+    let subject2 = document.querySelector("#subject2").value;
+    let subject3 = document.querySelector("#subject3").value;
+    let subject4 = document.querySelector("#subject4").value;
+    let subject5 = document.querySelector("#subject5").value;
+    let subject6 = document.querySelector("#subject6").value;
+    let subject7 = document.querySelector("#subject7").value;
+    let subject8 = document.querySelector("#subject8").value;
+    let subject9 = document.querySelector("#subject9").value;
+    let subject10 = document.querySelector("#subject10").value;
+
+    //   then Add all subjects value
+
+    let totalMarks = parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1) +
+        parseInt(subject1);
+
+    document.querySelector("#totalMarks").innerHTML = `Total Marks ${totalMarks}`
+
+    let result = totalMarks / 10;
+
+    document.querySelector("#result").innerHTML = `You have got  ${result}%`;
+}
