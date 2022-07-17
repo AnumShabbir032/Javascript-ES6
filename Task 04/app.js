afb52f4054567289434f4f29840a9b3665e7758b
AnumShabbir032 <anumshabbir032@gmail.com>
2022-07-17 21:42:23 +0500

    Javascript-Es6

--- Task 04/app.js
+++ Task 04/app.js
@@ -1,34 +1,86 @@
+// subject 1 result
 function chekper1() {
     let marks_1 = document.querySelector("#marks_1").value;
     let subperc_1 = marks_1 / 100 * 100;
-    document.querySelector("#subperc_1").innerHTML = `${subperc_1}%`;
+    let subperc = subperc_1.toFixed(2);
 
+
+    if(marks_1 > 100){
+        document.querySelector("#subperc_1").innerHTML  = `Invalid Mark`;
+    }
+    else if(marks_1 == ""){
+        document.querySelector("#subperc_1").innerHTML  = `Input is empty`;
+    }
+    else{
+        document.querySelector("#subperc_1").innerHTML = `${subperc}%`;
+    }
 }
-
+// subject 2 result
 function chekper2() {
     let marks_2 = document.querySelector("#marks_2").value;
     let subperc_2 = marks_2 / 100 * 100;
-    document.querySelector("#subperc_2").innerHTML = `${subperc_2}%`;
+    let subperc = subperc_2.toFixed(2);
+    
+    if(marks_2 > 100){
+        document.querySelector("#subperc_2").innerHTML  = `Invalid Mark`;
+    }
+    else if(marks_2 == ""){
+        document.querySelector("#subperc_2").innerHTML  = `Input is empty`;
+    }
+    else{
+        document.querySelector("#subperc_2").innerHTML = `${subperc}%`;
+    }
 }
-
+// subject 3 result
 function chekper3() {
     let marks_3 = document.querySelector("#marks_3").value;
     let subperc_3 = marks_3 / 100 * 100;
-    document.querySelector("#subperc_3").innerHTML = `${subperc_3}%`;
-}
+    let subperc = subperc_3.toFixed(2);
 
+    if(marks_3 > 100){
+        document.querySelector("#subperc_3").innerHTML  = `Invalid Mark`;
+    }
+    else if(marks_3 == ""){
+        document.querySelector("#subperc_3").innerHTML  = `Input is empty`;
+    }
+    else{
+        document.querySelector("#subperc_3").innerHTML = `${subperc}%`;
+    }
+}
+// subject 4 result
 function chekper4() {
     let marks_4 = document.querySelector("#marks_4").value;
     let subperc_4 = marks_4 / 100 * 100;
-    document.querySelector("#subperc_4").innerHTML = `${subperc_4}%`;
-}
+    let subperc = subperc_4.toFixed(2);
 
+    if(marks_4 > 100){
+        document.querySelector("#subperc_4").innerHTML  = `Invalid Mark`;
+    }
+    else if(marks_4 == ""){
+        document.querySelector("#subperc_4").innerHTML  = `Input is empty`;
+    }
+    else{
+        document.querySelector("#subperc_4").innerHTML = `${subperc}%`;
+    }
+}
+// subject 5 result
 function chekper5() {
     let marks_5 = document.querySelector("#marks_5").value;
     let subperc_5 = marks_5 / 100 * 100;
-    document.querySelector("#subperc_5").innerHTML = `${subperc_5}%`;
+    let subperc = subperc_5.toFixed(2);
 
+    if(marks_5 > 100){
+        document.querySelector("#subperc_5").innerHTML  = `Invalid Mark`;
+    }
+    else if(marks_5 == ""){
+        document.querySelector("#subperc_5").innerHTML  = `Input is empty`;
+    }
+    else{
+        document.querySelector("#subperc_5").innerHTML = `${subperc}%`;
+    }
 }
+
+// Function for Percentage
 
 function percentage() {
     let marks_1 = document.querySelector("#marks_1").value;
@@ -43,53 +95,48 @@ function percentage() {
         parseInt(marks_4) +
         parseInt(marks_5);
 
-    document.querySelector("#obtMarks").innerHTML = `${ObtainedMarks}`;
+// Validation on Obtained Marks result
 
-    var percentage = (ObtainedMarks / 500) * 100;
+    if(ObtainedMarks > 501){
+        document.querySelector("#obtMarks").innerHTML = `Invalid Marks`;
+    }
+    else if(marks_1 == "" && marks_2 ==""||  marks_3 == "" && marks_4 == "" || marks_5 == ""){
+        document.querySelector("#obtMarks").innerHTML = `Fill all inputs first`;
+    }
+    else{
+        document.querySelector("#obtMarks").innerHTML = `${ObtainedMarks}`;
+    }
 
-    document.querySelector("#percentage").innerHTML = `${percentage}%`;
+
+  
+// Percentage Formula
+    var percentageValue = (ObtainedMarks / 500) * 100;
+
+//The toFixed() method formats a number using fixed-point notation.
+
+    let percentage = percentageValue.toFixed(2);
 
 
+//  Conditions On Percentage Grading   
     if (percentage >= 80) {
-        document.querySelector("#percentage").innerHTML = `${percentage}%  A+`;
+        document.querySelector("#percentage").innerHTML = `${percentage}%  (A+)`;
         } 
         else if (percentage >= 70 && percentage < 80) {
-        document.querySelector("#percentage").innerHTML = `${percentage}% A`;
+        document.querySelector("#percentage").innerHTML = `${percentage}% (A)`;
         }
          else if (percentage >= 60 && percentage < 70) {
-            document.querySelector("#percentage").innerHTML = `${percentage}% B`;
+            document.querySelector("#percentage").innerHTML = `${percentage}% (B)`;
         } 
         else if(percentage >= 50 && percentage < 60){
-            document.querySelector("#percentage").innerHTML = `${percentage}% C`;
+            document.querySelector("#percentage").innerHTML = `${percentage}% (C)`;
         }
         else if(percentage >= 40 && percentage < 50){
-            document.querySelector("#percentage").innerHTML = `${percentage}% D`;
+            document.querySelector("#percentage").innerHTML = `${percentage}% (D)`;
         }
-        else if(percentage >= 100){
-                document.querySelector("#percentage").innerHTML = `Invailid`;
+        else if(percentage > ""){
+            document.querySelector("#percentage").innerHTML = `Invalid Result`;
         }
         else{
-            document.querySelector("#percentage").innerHTML = `${percentage}% Fail`;
+            document.querySelector("#percentage").innerHTML = `${percentage}% (Fail)`;
         }
-
-
-    
 }
-// if (aver < 60) {
-    //           alert( "Fail");
-    //       }
-    //       else if (aver < 70){
-    //         alert( "Grade D");
-    //       }
-    //       else if (aver < 80){
-    //         alert( "Grade C");
-    //       }
-    //       else if (aver < 90){
-    //         alert( "Grade B");
-    //       }
-    //       else if (aver < 100){
-    //         alert( "Grade A+");
-    //       }
-    //       else{
-    //           alert(aver);
-    //       }
